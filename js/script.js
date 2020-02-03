16e2efcc806506a563e22d5c5cf2feedea993cc0
Hanna Altunina <hanna.aljaxeeva@gmail.com>
2020-02-03 15:44:12 +0300

    правки навигации

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-var mainNav = document.querySelector('.main-nav__list');
-var navToggle = document.querySelector('.main-nav__toggle--opened');
-
-navToggle.classList.remove('main-nav__toggle--nojs');
+var mainNav = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.header__toggle');
 
 navToggle.addEventListener('click', function() {
-  if (navToggle.classList.contains('main-nav__toggle--opened')) {
-    mainNav.classList.remove('main-nav');
-    mainNav.classList.add('main-nav__list--closed');
-    navToggle.classList.remove('main-nav__toggle--opened');
-    navToggle.classList.add('main-nav__toggle--closed');
+  if (navToggle.classList.contains('header__toggle--open')) {
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
+    navToggle.classList.remove('header__toggle--open');
+    navToggle.classList.add('header__toggle--close');
   } else {
     mainNav.classList.add('main-nav');
-    mainNav.classList.remove('main-nav__list--closed');
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navToggle.classList.add('main-nav__toggle--opened');
+    mainNav.classList.remove('main-nav--closed');
+    navToggle.classList.remove('header__toggle--close');
+    navToggle.classList.add('header__toggle--open');
   }
 });
